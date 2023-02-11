e05ad538b80f9c31777479ad4ddfab13b6eb512b
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-02-11 21:05:19 +0600

    Cast Details - 20%

--- src/components/castDetails/CastDetails.js
+++ src/components/castDetails/CastDetails.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import Card from "react-bootstrap/Card";
+import "./castDetails.css";
 
 const CastDetails = () => {
-  return <div>castDetails</div>;
+  const [characters, setCharacter] = useState([]);
+  const [episodes, setEpisode] = useState([]);
+  const [locations, setLocation] = useState([]);
+
+  // Characters data Fetching
+  const getChar = async () => {
+    const url = "https://rickandmortyapi.com/api/character";
+    const { data } = await axios.get(url);
+    console.log(data.results);
+    setCharacter(data.results);
+  };
+
+  useEffect(() => {
+    getChar();
+  }, []);
+
+  // Episode data Fetching
+  const getEpisode = async () => {
+    const url = "https://rickandmortyapi.com/api/episode";
+    const { data } = await axios.get(url);
+    console.log(data.results);
+    setEpisode(data.results);
+  };
+
+  useEffect(() => {
+    getEpisode();
+  }, []);
+
+  // Locations data Fetching
+  const getLocation = async () => {
+    const url = "https://rickandmortyapi.com/api/location";
+    const { data } = await axios.get(url);
+    console.log(data.results);
+    setLocation(data.results);
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <div className="castDetails-container">
+      <div className="cast-details">
+        <div className="cast-show">
+          {characters.map((item, index) => (
+            <Card className="card-container" key={index}>
+              <Card.Img variant="top" src={item.image} />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+        <div className="castDetails-show">
+          <h1>hi</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CastDetails;
