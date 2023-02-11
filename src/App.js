28995412abc0f3bca4f581d572929f1ab4f7279f
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-02-12 02:44:13 +0600

    FullCast and CastDetails Components routed by RRDOM

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Home from "./components/pages/Home";
+import CastDetails from "./components/castDetails/CastDetails";
+
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
+import FullCast from "./components/pages/fullcast/FullCast";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fullcast" element={<FullCast />} />
+        <Route path="/castDetails/:id" element={<CastDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
