9df77019f92537c3103bda2d55fbe26718e83cc1
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-02-12 02:46:22 +0600

    Slick Slider Added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,11 +10,27 @@ import portal from "../../images/HomePage/portal.png";
 import logo from "../../images/Logo.png";
 
 import "./Home.css";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Home = () => {
   const [characters, setCharacter] = useState([]);
   const [episodes, setEpisode] = useState([]);
   const [locations, setLocation] = useState([]);
+
+  const settings = {
+    dots: false,
+    centerMode: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+  };
+
+  // const [hideCast, setHideCast] = useState(false);
 
   // Characters data Fetching
   const getChar = async () => {
@@ -52,57 +68,88 @@ const Home = () => {
     getLocation();
   }, []);
 
-  const seeDetails = () => {};
   return (
     <div className="home-container">
-      <div
-        onClick={() => {
-          seeDetails();
-        }}
-        className="characters-container"
-      >
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+
+      {/* Character */}
+
+      <Slider {...settings}>
         {characters.map((item, index) => (
-          <Card className="card-container" key={index}>
-            <Card.Img variant="top" src={item.image} />
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-            </Card.Body>
-          </Card>
+          <Link to={`/castDetails/${item.id}`}>
+            <Card className="card-container" key={index}>
+              <Card.Img variant="top" src={item.image} />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Link>
         ))}
+      </Slider>
+      <div>
+        {
+          <div className="meet-the-cast-text">
+            <h4>Meet the Cast</h4>
+            <Link to="fullcast">
+              <Button
+              // onClick={() => {
+              //   setHideCast(characters.slice(0, 5));
+              //   setHideCast(true);
+              // }}
+              >
+                View All
+              </Button>
+            </Link>
+          </div>
+        }
+        <div className="characters-container">
+          {characters.map((item, index) => (
+            <Link to={`/castDetails/${item.id}`}>
+              <Card className="card-container" key={index}>
+                <Card.Img variant="top" src={item.image} />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Link>
+          ))}
+        </div>
       </div>
 
       {/* Episode */}
 
-      <div
-        onClick={() => {
-          seeDetails();
-        }}
-        className="episode-container"
-      >
-        {episodes.map((item, index) => (
-          <Card className="card-container" key={index}>
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))}
+      <div>
+        <div className="episodes-text">
+          <h4>Episodes</h4>
+        </div>
+        <div className="episode-container">
+          {episodes.map((item, index) => (
+            <Card className="card-container" key={index}>
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
       </div>
 
       {/* Locations */}
 
-      <div
-        onClick={() => {
-          seeDetails();
-        }}
-        className="episode-container"
-      >
-        {locations.map((item, index) => (
-          <Card className="card-container" key={index}>
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))}
+      <div>
+        <div className="locations-text">
+          <h4>Locations</h4>
+        </div>
+        <div className="location-container">
+          {locations.map((item, index) => (
+            <Card className="card-container" key={index}>
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
