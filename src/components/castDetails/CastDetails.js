83e2d9e548b05de41e43bdc9e379bb2dd8502c9d
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-02-12 02:39:38 +0600

    90% of Cast Details Page

--- src/components/castDetails/CastDetails.js
+++ src/components/castDetails/CastDetails.js
@@ -1,67 +1,140 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Status from "../../images/Icons/Status.png";
+import Gender from "../../images/Icons/Gender.png";
+import Location from "../../images/Icons/Location.png";
+import origin from "../../images/Icons/origin.png";
+import Redirect from "../../images/Icons/Redirect.png";
+import Species from "../../images/Icons/Species.png";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-import Card from "react-bootstrap/Card";
+// import Card from "react-bootstrap/Card";
 import "./castDetails.css";
+import { useParams } from "react-router-dom";
 
 const CastDetails = () => {
-  const [characters, setCharacter] = useState([]);
-  const [episodes, setEpisode] = useState([]);
-  const [locations, setLocation] = useState([]);
+  const [character, setCharacter] = useState();
+  console.log(character);
+
+  const params = useParams();
+  const castId = params.id;
+  console.log({ castId });
 
   // Characters data Fetching
   const getChar = async () => {
-    const url = "https://rickandmortyapi.com/api/character";
+    const url = `https://rickandmortyapi.com/api/character/${castId}`;
     const { data } = await axios.get(url);
-    console.log(data.results);
-    setCharacter(data.results);
+    console.log(data);
+    setCharacter(data);
   };
 
   useEffect(() => {
     getChar();
   }, []);
 
-  // Episode data Fetching
-  const getEpisode = async () => {
-    const url = "https://rickandmortyapi.com/api/episode";
-    const { data } = await axios.get(url);
-    console.log(data.results);
-    setEpisode(data.results);
-  };
-
-  useEffect(() => {
-    getEpisode();
-  }, []);
-
-  // Locations data Fetching
-  const getLocation = async () => {
-    const url = "https://rickandmortyapi.com/api/location";
-    const { data } = await axios.get(url);
-    console.log(data.results);
-    setLocation(data.results);
-  };
-
-  useEffect(() => {
-    getLocation();
-  }, []);
-
   return (
     <div className="castDetails-container">
-      <div className="cast-details">
-        <div className="cast-show">
-          {characters.map((item, index) => (
-            <Card className="card-container" key={index}>
-              <Card.Img variant="top" src={item.image} />
-              <Card.Body>
-                <Card.Title>{item.name}</Card.Title>
-              </Card.Body>
-            </Card>
-          ))}
+      {character && (
+        <div className="cast-details">
+          <div className="left-side">
+            <h2 className="character-name">{character.name}</h2>
+            <div className="character-img theme-border">
+              <img src={character.image} alt="" />
+            </div>
+          </div>
+
+          <div className="right-side">
+            <div className="first-part">
+              <div className="first-part-child theme-border">
+                <div className="details-icon">
+                  <img src={Status} alt="" />
+                </div>
+                <div>
+                  <span>Status</span> <br /> <span>{character.status}</span>
+                </div>
+              </div>
+
+              <div className="first-part-child theme-border">
+                <div className="details-icon">
+                  <img src={Species} alt="" />
+                </div>
+                <div>
+                  <span>Species</span> <br /> <span>{character.species}</span>
+                </div>
+              </div>
+
+              <div className="first-part-child theme-border">
+                <div className="details-icon">
+                  <img src={Gender} alt="" />
+                </div>
+                <div>
+                  <span>
+                    <small>Gender</small>
+                  </span>
+                  <br /> <span>{character.gender}</span>
+                </div>
+              </div>
+            </div>
+            <div className="second-part theme-border">
+              <div className="details-icon">
+                <img src={origin} alt="" />
+              </div>
+              <div>
+                <div>
+                  <span>Origin</span> <br />
+                  <div className="second-part-lower">
+                    <span>{character?.origin?.name}</span>
+                    <div className="share-icon">
+                      <img src={Redirect} alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="third-part theme-border">
+              <div className="details-icon">
+                <img src={Location} alt="" />
+              </div>
+              <div>
+                <div>
+                  <span>Last Known Location</span> <br />
+                  <div className="second-part-lower">
+                    <span>{character?.origin?.name}</span>
+                    <div className="share-icon">
+                      <img src={Redirect} alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="fourth-part theme-border">
+              <div className="details-icon">
+                <img src={Status} alt="" />
+              </div>
+              <div>
+                <div>
+                  <span>Episodes (S)</span> <br />
+                  <div className="fourth-part-lower">
+                    <ul>
+                      {character.episode &&
+                        character.episode.map((epd, index) => <li>{epd}</li>)}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <div className="cast-show">
+  <Card className="card-container">
+    <Card.Body>
+      <Card.Title>{character.name}</Card.Title>
+    </Card.Body>
+    <Card.Img variant="top" src={character.image} />
+  </Card>
+</div> */}
+          {/* <div className="castDetails-show">.</div> */}
         </div>
-        <div className="castDetails-show">
-          <h1>hi</h1>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
