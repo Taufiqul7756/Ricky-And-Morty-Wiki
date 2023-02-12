3417ef5dbbddfd2ec4f964cca12e42adb4434fce
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-02-13 03:34:34 +0600

    Responsiveness Done for mobile

--- src/components/pages/fullcast/FullCast.js
+++ src/components/pages/fullcast/FullCast.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import logo from "../../../images/Logo.png";
 
 import "./fullcast.css";
+import "../Home.css";
 
 export default function FullCast() {
   const [characters, setCharacter] = useState([]);
@@ -28,21 +29,27 @@ export default function FullCast() {
         </div>
       </Link>
 
-      <span className="the-cast-text">
-        <h2>The Cast</h2>
-      </span>
-      <div className="full-cast-container ">
-        {characters &&
-          characters.map((item, index) => (
-            <Link to={`/castDetails/${item.id}`}>
-              <Card className="card-container theme-border" key={index}>
-                <Card.Img variant="top" src={item.image} />
-                <Card.Body>
-                  <Card.Title>{item.name}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Link>
-          ))}
+      <div className="container">
+        <span className="the-cast-text">
+          <h2>The Cast</h2>
+        </span>
+        <div className="full-cast-container ">
+          {characters &&
+            characters.map((item, index) => (
+              <Link to={`/castDetails/${item.id}`}>
+                <Card className="card-container theme-border" key={index}>
+                  <Card.Img
+                    className="card-img"
+                    variant="top"
+                    src={item.image}
+                  />
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Link>
+            ))}
+        </div>
       </div>
     </div>
   );
